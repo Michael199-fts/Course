430dd2c3d1a54f8fc1d07d8e035a42fea5fe5907
Michael199-fts <79326644+Michael199-fts@users.noreply.github.com>
2021-03-23 22:59:54 +0300

    Add files via upload

--- 12.py
+++ 12.py
@@ -1,9 +1,8 @@
 #Задание №12
-class JellyBean:
-    def __init__(self, name, calories, flavor):
+class Dessert:
+    def __init__(self, name, calories):
         self.name = name
         self.calories = calories
-        self.flavor = flavor
 
     def set_calories(self, calories):
         self.calories = calories
@@ -17,12 +16,6 @@ class JellyBean:
     def get_name(self):
         return self.name
 
-    def set_flavor(self, flavor):
-        self.flavor = flavor
-
-    def get_flavor(self):
-        return self.flavor
-
     def is_healty(self):
         if self.calories < 200:
             return True
@@ -30,12 +23,17 @@ class JellyBean:
             return False
 
     def is_delicious(self):
+            return True
+
+class JellyBean(Dessert):
+    def set_flavor(self, flavor):
+        self.flavor = flavor
+
+    def get_flavor(self):
+        return self.flavor
+
+    def is_delicious(self):
         if self.flavor.lower() == 'black licorice':
             return False
         else:
             return True
-"""
-Dessert = JellyBean('Black Licorice', 201, "black licorice")
-print(Cake.is_delicious())
-print(Cake.is_healty())
-"""
