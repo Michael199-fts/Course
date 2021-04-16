1973a00d82cfa94d0d4834b299773907029ea8fd
Michael199-fts <79326644+Michael199-fts@users.noreply.github.com>
2021-04-16 17:07:56 +0300

    Add files via upload

--- task6.py
+++ task6.py
@@ -1,10 +1,15 @@
 #Задание №6
+
+class WrongNumberOfPlayersError(Exception):
+    pass
+class NoSuchStrategyError(Exception):
+    pass
 def rps_game_winner(players_mass):
     if len(players_mass) > 2:
-        raise Exception('WrongNumberOfPlayersError')
+        raise WrongNumberOfPlayersError('WrongNumberOfPlayersError')
 #исключение в случае, когда слишком много игроков
     if players_mass[0][1] != 'P' and players_mass[0][1] != 'S' and players_mass[0][1] != 'R' or players_mass[1][1] != 'P' and players_mass[1][1] != 'S' and players_mass[1][1] != 'R':
-        raise Exception("NoSuchStrategyError")
+        raise NoSuchStrategyError("NoSuchStrategyError")
 #исключение в случае, когда подаются неверные значения
     if players_mass[0][1] == 'P' and players_mass[1][1] == 'R':
         return players_mass[0]
@@ -21,9 +26,8 @@ def rps_game_winner(players_mass):
     if players_mass[0][1] == players_mass[1][1]:
         return players_mass[0]
 #формирование ответа на вариант исхода игры
-"""
+
 print(rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']]))
 print(rps_game_winner([['player1', 'P'], ['player2', 'A']]))
 print(rps_game_winner([['player1', 'P'], ['player2', 'S']]))
 print(rps_game_winner([['player1', 'P'], ['player2', 'P']]))
-"""
