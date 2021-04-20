bc8eed1d5e219aeaf69b2007b74846cc70e2f704
Michael199-fts <79326644+Michael199-fts@users.noreply.github.com>
2021-04-20 10:48:58 +0300

    Add files via upload

--- task6.py
+++ task6.py
@@ -4,30 +4,33 @@ class WrongNumberOfPlayersError(Exception):
     pass
 class NoSuchStrategyError(Exception):
     pass
-def rps_game_winner(players_mass):
-    if len(players_mass) > 2:
+def rps_game_winner(players_mass=None):
+    if players_mass == None:
+        return None
+    if len(players_mass) != 2:
         raise WrongNumberOfPlayersError('WrongNumberOfPlayersError')
 #исключение в случае, когда слишком много игроков
     if players_mass[0][1] != 'P' and players_mass[0][1] != 'S' and players_mass[0][1] != 'R' or players_mass[1][1] != 'P' and players_mass[1][1] != 'S' and players_mass[1][1] != 'R':
         raise NoSuchStrategyError("NoSuchStrategyError")
 #исключение в случае, когда подаются неверные значения
     if players_mass[0][1] == 'P' and players_mass[1][1] == 'R':
-        return players_mass[0]
+        return str(players_mass[0][0]) + ', ' + str(players_mass[0][1])
     if players_mass[0][1] == 'R' and players_mass[1][1] == 'S':
-        return players_mass[0]
+        return str(players_mass[0][0]) + ', ' + str(players_mass[0][1])
     if players_mass[0][1] == 'S' and players_mass[1][1] == 'P':
-        return players_mass[0]
+        return str(players_mass[0][0]) + ', ' + str(players_mass[0][1])
     if players_mass[0][1] == 'R' and players_mass[1][1] == 'P':
-        return players_mass[1]
+        return str(players_mass[1][0]) + ', ' + str(players_mass[1][1])
     if players_mass[0][1] == 'P' and players_mass[1][1] == 'S':
-        return players_mass[1]
+        return str(players_mass[1][0]) + ', ' + str(players_mass[1][1])
     if players_mass[0][1] == 'S' and players_mass[1][1] == 'R':
-        return players_mass[1]
+        return str(players_mass[1][0]) + ', ' + str(players_mass[1][1])
     if players_mass[0][1] == players_mass[1][1]:
-        return players_mass[0]
+        return str(players_mass[0][0]) + ', ' + str(players_mass[0][1])
 #формирование ответа на вариант исхода игры
 
-print(rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']]))
-print(rps_game_winner([['player1', 'P'], ['player2', 'A']]))
+#print(rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']]))
+#print(rps_game_winner([['player1', 'P'], ['player2', 'A']]))
 print(rps_game_winner([['player1', 'P'], ['player2', 'S']]))
 print(rps_game_winner([['player1', 'P'], ['player2', 'P']]))
+print(rps_game_winner())
