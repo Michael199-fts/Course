54a199d4223c4914c7a0825d23eb8f44b3dbde18
Michael199-fts <79326644+Michael199-fts@users.noreply.github.com>
2021-04-21 19:10:41 +0300

    Add files via upload

--- task6.py
+++ task6.py
@@ -14,19 +14,19 @@ def rps_game_winner(players_mass=None):
         raise NoSuchStrategyError("NoSuchStrategyError")
 #исключение в случае, когда подаются неверные значения
     if players_mass[0][1] == 'P' and players_mass[1][1] == 'R':
-        return str(players_mass[0][0]) + ', ' + str(players_mass[0][1])
+        return str(players_mass[0][0]) + ' ' + str(players_mass[0][1])
     if players_mass[0][1] == 'R' and players_mass[1][1] == 'S':
-        return str(players_mass[0][0]) + ', ' + str(players_mass[0][1])
+        return str(players_mass[0][0]) + ' '+ str(players_mass[0][1])
     if players_mass[0][1] == 'S' and players_mass[1][1] == 'P':
-        return str(players_mass[0][0]) + ', ' + str(players_mass[0][1])
+        return str(players_mass[0][0]) + ' '+ str(players_mass[0][1])
     if players_mass[0][1] == 'R' and players_mass[1][1] == 'P':
-        return str(players_mass[1][0]) + ', ' + str(players_mass[1][1])
+        return str(players_mass[1][0]) + ' '+ str(players_mass[1][1])
     if players_mass[0][1] == 'P' and players_mass[1][1] == 'S':
-        return str(players_mass[1][0]) + ', ' + str(players_mass[1][1])
+        return str(players_mass[1][0]) + ' '+ str(players_mass[1][1])
     if players_mass[0][1] == 'S' and players_mass[1][1] == 'R':
-        return str(players_mass[1][0]) + ', ' + str(players_mass[1][1])
+        return str(players_mass[1][0]) + ' '+ str(players_mass[1][1])
     if players_mass[0][1] == players_mass[1][1]:
-        return str(players_mass[0][0]) + ', ' + str(players_mass[0][1])
+        return str(players_mass[0][0]) + ' '+ str(players_mass[0][1])
 #формирование ответа на вариант исхода игры
 
 #print(rps_game_winner([['player1', 'P'], ['player2', 'S'], ['player3', 'S']]))
